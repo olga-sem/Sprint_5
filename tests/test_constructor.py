3f2979731785f9f0aef572da3b14e63a86572f42
Olga Semenova <semenovaolga-2010@mail.ru>
2024-11-03 03:42:29 +0300

    second_edition

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,13 +1,14 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import TestLocators
+import data_for_tests
 
 class TestConstructor:
-    def test_open_constructor_by_clicking_its_button(self, driver):
+    def test_open_constructor_by_clicking_its_button(self, driver, email):
         driver.find_element(*TestLocators.PERSONAL_ACCOUNT_BUTTON).click()
         WebDriverWait(driver, 3).until(EC.visibility_of_element_located(TestLocators.SIGN_IN_FORM))
-        driver.find_element(*TestLocators.EMAIL_FIELD_IN_AUTH_FORM).send_keys('olga_semenova_12_666@gmail.com')
-        driver.find_element(*TestLocators.PASSWORD_FIELD_IN_AUTH_FORM).send_keys('567892')
+        driver.find_element(*TestLocators.EMAIL_FIELD_IN_AUTH_FORM).send_keys(email)
+        driver.find_element(*TestLocators.PASSWORD_FIELD_IN_AUTH_FORM).send_keys(data_for_tests.password)
         driver.find_element(*TestLocators.SIGN_IN_BUTTON_IN_AUTH_FORM).click()
         WebDriverWait(driver, 5).until(EC.visibility_of_element_located(TestLocators.ORDER_BUTTON))
         driver.find_element(*TestLocators.PERSONAL_ACCOUNT_BUTTON).click()
@@ -16,11 +17,11 @@ class TestConstructor:
         WebDriverWait(driver, 3).until(EC.visibility_of_element_located(TestLocators.CONSTRUCT_A_BURGER_LINE))
         assert driver.find_element(*TestLocators.CONSTRUCT_A_BURGER_LINE)
 
-    def test_open_constructor_by_clicking_logo(self, driver):
+    def test_open_constructor_by_clicking_logo(self, driver, email):
         driver.find_element(*TestLocators.PERSONAL_ACCOUNT_BUTTON).click()
         WebDriverWait(driver, 3).until(EC.visibility_of_element_located(TestLocators.SIGN_IN_FORM))
-        driver.find_element(*TestLocators.EMAIL_FIELD_IN_AUTH_FORM).send_keys('olga_semenova_12_666@gmail.com')
-        driver.find_element(*TestLocators.PASSWORD_FIELD_IN_AUTH_FORM).send_keys('567892')
+        driver.find_element(*TestLocators.EMAIL_FIELD_IN_AUTH_FORM).send_keys(email)
+        driver.find_element(*TestLocators.PASSWORD_FIELD_IN_AUTH_FORM).send_keys(data_for_tests.password)
         driver.find_element(*TestLocators.SIGN_IN_BUTTON_IN_AUTH_FORM).click()
         WebDriverWait(driver, 5).until(EC.visibility_of_element_located(TestLocators.ORDER_BUTTON))
         driver.find_element(*TestLocators.PERSONAL_ACCOUNT_BUTTON).click()
@@ -29,11 +30,11 @@ class TestConstructor:
         WebDriverWait(driver, 3).until(EC.visibility_of_element_located(TestLocators.CONSTRUCT_A_BURGER_LINE))
         assert driver.find_element(*TestLocators.CONSTRUCT_A_BURGER_LINE)
 
-    def test_open_sauces_section(self, driver):
+    def test_open_sauces_section(self, driver, email):
         driver.find_element(*TestLocators.PERSONAL_ACCOUNT_BUTTON).click()
         WebDriverWait(driver, 3).until(EC.visibility_of_element_located(TestLocators.SIGN_IN_FORM))
-        driver.find_element(*TestLocators.EMAIL_FIELD_IN_AUTH_FORM).send_keys('olga_semenova_12_666@gmail.com')
-        driver.find_element(*TestLocators.PASSWORD_FIELD_IN_AUTH_FORM).send_keys('567892')
+        driver.find_element(*TestLocators.EMAIL_FIELD_IN_AUTH_FORM).send_keys(email)
+        driver.find_element(*TestLocators.PASSWORD_FIELD_IN_AUTH_FORM).send_keys(data_for_tests.password)
         driver.find_element(*TestLocators.SIGN_IN_BUTTON_IN_AUTH_FORM).click()
         WebDriverWait(driver, 5).until(EC.visibility_of_element_located(TestLocators.ORDER_BUTTON))
         driver.find_element(*TestLocators.PERSONAL_ACCOUNT_BUTTON).click()
@@ -44,11 +45,11 @@ class TestConstructor:
         WebDriverWait(driver, 3).until(EC.visibility_of_element_located(TestLocators.SAUCES_CHOSEN_BUTTON))
         assert driver.find_element(*TestLocators.SAUCES_CHOSEN_BUTTON)
 
-    def test_open_buns_section(self, driver):
+    def test_open_buns_section(self, driver, email):
         driver.find_element(*TestLocators.PERSONAL_ACCOUNT_BUTTON).click()
         WebDriverWait(driver, 3).until(EC.visibility_of_element_located(TestLocators.SIGN_IN_FORM))
-        driver.find_element(*TestLocators.EMAIL_FIELD_IN_AUTH_FORM).send_keys('olga_semenova_12_666@gmail.com')
-        driver.find_element(*TestLocators.PASSWORD_FIELD_IN_AUTH_FORM).send_keys('567892')
+        driver.find_element(*TestLocators.EMAIL_FIELD_IN_AUTH_FORM).send_keys(email)
+        driver.find_element(*TestLocators.PASSWORD_FIELD_IN_AUTH_FORM).send_keys(data_for_tests.password)
         driver.find_element(*TestLocators.SIGN_IN_BUTTON_IN_AUTH_FORM).click()
         WebDriverWait(driver, 5).until(EC.visibility_of_element_located(TestLocators.ORDER_BUTTON))
         driver.find_element(*TestLocators.PERSONAL_ACCOUNT_BUTTON).click()
@@ -60,11 +61,11 @@ class TestConstructor:
         WebDriverWait(driver, 3).until(EC.visibility_of_element_located(TestLocators.BUNS_CHOSEN_BUTTON))
         assert driver.find_element(*TestLocators.BUNS_CHOSEN_BUTTON)
 
-    def test_open_fillings_section(self, driver):
+    def test_open_fillings_section(self, driver, email):
         driver.find_element(*TestLocators.PERSONAL_ACCOUNT_BUTTON).click()
         WebDriverWait(driver, 3).until(EC.visibility_of_element_located(TestLocators.SIGN_IN_FORM))
-        driver.find_element(*TestLocators.EMAIL_FIELD_IN_AUTH_FORM).send_keys('olga_semenova_12_666@gmail.com')
-        driver.find_element(*TestLocators.PASSWORD_FIELD_IN_AUTH_FORM).send_keys('567892')
+        driver.find_element(*TestLocators.EMAIL_FIELD_IN_AUTH_FORM).send_keys(email)
+        driver.find_element(*TestLocators.PASSWORD_FIELD_IN_AUTH_FORM).send_keys(data_for_tests.password)
         driver.find_element(*TestLocators.SIGN_IN_BUTTON_IN_AUTH_FORM).click()
         WebDriverWait(driver, 5).until(EC.visibility_of_element_located(TestLocators.ORDER_BUTTON))
         driver.find_element(*TestLocators.PERSONAL_ACCOUNT_BUTTON).click()
--- tests/test_log_out.py
+++ tests/test_log_out.py
@@ -1,13 +1,14 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import TestLocators
+import data_for_tests
 
 class TestLogOut:
-    def test_log_out_with_exit_button(self, driver):
+    def test_log_out_with_exit_button(self, driver, email):
         driver.find_element(*TestLocators.PERSONAL_ACCOUNT_BUTTON).click()
         WebDriverWait(driver, 3).until(EC.visibility_of_element_located(TestLocators.SIGN_IN_FORM))
-        driver.find_element(*TestLocators.EMAIL_FIELD_IN_AUTH_FORM).send_keys('olga_semenova_12_666@gmail.com')
-        driver.find_element(*TestLocators.PASSWORD_FIELD_IN_AUTH_FORM).send_keys('567892')
+        driver.find_element(*TestLocators.EMAIL_FIELD_IN_AUTH_FORM).send_keys(email)
+        driver.find_element(*TestLocators.PASSWORD_FIELD_IN_AUTH_FORM).send_keys(data_for_tests.password)
         driver.find_element(*TestLocators.SIGN_IN_BUTTON_IN_AUTH_FORM).click()
         WebDriverWait(driver, 5).until(EC.visibility_of_element_located(TestLocators.ORDER_BUTTON))
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable(TestLocators.PERSONAL_ACCOUNT_BUTTON))
